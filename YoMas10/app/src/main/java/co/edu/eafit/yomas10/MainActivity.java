de3c3073cea46651975fe44e7d169e59dec208c1
aleochoam <alejo8a_2@hotmail.com>
2015-09-11 00:06:51 -0500

    peleando con las notificaciones

--- YoMas10/app/src/main/java/co/edu/eafit/yomas10/MainActivity.java
+++ YoMas10/app/src/main/java/co/edu/eafit/yomas10/MainActivity.java
@@ -1,23 +1,48 @@
 package co.edu.eafit.yomas10;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
+import com.parse.ParsePush;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sendInv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Context ctx = this;
+
         Parse.initialize(this, "pugXkDo33e0MsbWMTsL5EChTBx3jKyWoDYI6ZE40", "rxxO31IwOiUHan1udkZjFwHgEIOJPI9zvPWGxm52");
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        ParsePush.subscribeInBackground("Canal1");
+
+        sendInv = (Button) findViewById(R.id.ntfyBt);
+        sendInv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParsePush push = new ParsePush();
+                push.setChannel("Canal1");
+                push.setMessage("Test desde la app");
+                push.sendInBackground();
+                Toast.makeText(ctx, "Se ha mandado la notificacion", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
