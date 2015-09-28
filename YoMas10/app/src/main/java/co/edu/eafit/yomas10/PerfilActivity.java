ee5c02df4492c32710f2938a6e2d39b00eb89319
aleochoam <alejo8a_2@hotmail.com>
2015-09-28 16:36:47 -0500

    editar perfil

--- YoMas10/app/src/main/java/co/edu/eafit/yomas10/PerfilActivity.java
+++ YoMas10/app/src/main/java/co/edu/eafit/yomas10/PerfilActivity.java
@@ -73,6 +73,9 @@ public class PerfilActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if (id == R.id.action_edit) {
+            Intent in = new Intent(this, EditarPerfilActivity.class);
+            startActivity(in);
         }
 
         return super.onOptionsItemSelected(item);
