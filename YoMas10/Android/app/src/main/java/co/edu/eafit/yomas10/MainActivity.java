85a906f97706d5a24b34d27be4f0edab81d4fc80
Jose Luis Alvarez <jalvar53@Jose.Chepe>
2015-10-05 15:50:58 -0500

    Swipe View en el MainActivity, Todas las clases en una

--- YoMas10/Android/app/src/main/java/co/edu/eafit/yomas10/MainActivity.java
+++ YoMas10/Android/app/src/main/java/co/edu/eafit/yomas10/MainActivity.java
@@ -3,50 +3,56 @@ package co.edu.eafit.yomas10;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.support.v4.app.Fragment;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
 
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
-import co.edu.eafit.yomas10.Clases.Equipo;
 import co.edu.eafit.yomas10.Clases.Jugador;
 import co.edu.eafit.yomas10.Helpers.StaticUser;
 
 
 public class MainActivity extends FragmentActivity {
 
-    ListView lista;
+    //ListView lista;
     private Jugador user;
-    private String arregloCadenas[] = {"Sistemas", "Psicologia"};
+    //private String arregloCadenas[] = {"Sistemas", "Psicologia"};
     ViewPager viewPager;
+    public List<String> fragments = new Vector<>();
+    static MyPagerAdapter myPagerAdapter;
+    final Context ctx = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragments.add(Frag1.class.getName());
+        fragments.add(Frag2.class.getName());
+
         viewPager = (ViewPager) findViewById(R.id.pager);
-        co.edu.eafit.yomas10.Helpers.PagerAdapter padaptder = new co.edu.eafit.yomas10.Helpers.PagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(padaptder);
+        myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(myPagerAdapter);
 
         /*lista = (ListView) findViewById(R.id.pager);
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arregloCadenas);
         lista.setAdapter(adaptador); */
         StaticUser.initialize();
 
-        final Context ctx = this;
         //canal = (EditText) findViewById(R.id.equipo);
 
 
@@ -89,6 +95,56 @@ public class MainActivity extends FragmentActivity {
         ParsePush.subscribeInBackground(canal.getText().toString().toLowerCase());
         Toast.makeText(this, getString(R.string.suscripcion) + " " +canal.getText().toString(),Toast.LENGTH_LONG).show();
     } */
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public List<String> fragmentsA;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            fragmentsA = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return Fragment.instantiate(ctx, Frag1.class.getName());
+                case 1:
+                    return Fragment.instantiate(ctx, Frag2.class.getName());
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+    public static class Frag1 extends Fragment {
+
+        public Frag1(){}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.frag_one, container, false);
+        }
+    }
+
+    public static class Frag2 extends Fragment {
+
+        public Frag2(){}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.frag_two, container, false);
+        }
+
+    }
 }
+
+
 
 //clave de Parse LO5XvpbqZL7cLu3vvKWpKZbTr9b3Cc21DU1pd7h3
