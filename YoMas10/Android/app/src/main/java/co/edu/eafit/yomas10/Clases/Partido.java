3893f74a8b91b0ce68ad9f67e353bfe903581bd9
Jose Luis Alvarez <jalvar53@Jose.Chepe>
2015-10-12 13:24:09 -0500

    Clase Partido

--- YoMas10/Android/app/src/main/java/co/edu/eafit/yomas10/Clases/Partido.java
+++ YoMas10/Android/app/src/main/java/co/edu/eafit/yomas10/Clases/Partido.java
@@ -4,4 +4,50 @@ package co.edu.eafit.yomas10.Clases;
  * Created by Usuario on 10/10/2015.
  */
 public class Partido {
+
+    private Equipo equipo1;
+    private Equipo equipo2;
+    private String nombre;
+    private String fecha;
+    private String hora;
+
+    public Equipo getEquipo1(){
+        return equipo1;
+    }
+
+    public Equipo getEquipo2(){
+        return equipo2;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getFecha(){
+        return fecha;
+    }
+
+    public String getHora(){
+        return hora;
+    }
+
+    public void setEquipo1(Equipo eq1){
+        this.equipo1 = eq1;
+    }
+
+    public void setEquipo2(Equipo eq2){
+        this.equipo2 = eq2;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setFecha(String fecha){
+        this.fecha = fecha;
+    }
+
+    public void setHora(String hora){
+        this.hora = hora;
+    }
 }
