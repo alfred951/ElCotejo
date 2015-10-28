fc4a27494cab345de4f3fb280986dfdcd52efdcc
Jose Luis Alvarez <jalvar53@Jose.Chepe>
2015-10-28 05:19:41 -0500

    Metodos Http Get y Http Post

---
+++ YoMas10/Android/app/src/main/java/co/edu/eafit/yomas10/Http.java
@@ -0,0 +1,78 @@
+package co.edu.eafit.yomas10;
+
+import android.app.Activity;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.Activity;
+
+public class Http{
+
+   static StringBuffer makeGetRequest(String url){
+       StringBuffer result = new StringBuffer();
+        try {
+
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
+
+            HttpResponse response = client.execute(request);
+
+            // receive response as inputStream
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            System.out.println(result.toString());
+            return result;
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+        return result;
+    }
+
+    static void makePostRequest(String url, List<NameValuePair> urlParameters) throws Exception{
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(url);
+
+            post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+            HttpResponse response = client.execute(post);
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            System.out.println(result.toString());
+        }
+        catch (Exception e){
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+    }
+}
